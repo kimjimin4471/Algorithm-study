f71acfa5b1c801fbbce442c01160b148018d2406
Kimjimin0807 <jm004471@naver.com>
2021-10-25 19:11:36 +0900

    벡준 가장 긴 감소하는 부분 수열

---
+++ 알고리즘 학습/11722.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int arr[1001];
+	int D[1001] = { 1, };
+	int n;
+	int max = 0;
+	cin >> n;
+	D[0] = 1;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i < n; i++) {
+		D[i] = 1;
+		for (int j = i - 1; j >= 0; j--) {
+			if (arr[i] < arr[j]) {
+				if (D[i] < D[j] + 1) {
+					D[i] = D[j] + 1;
+				}
+			}
+		}
+	}
+	max = D[0];
+	for (int i = 1; i < n; i++) {
+		if (max < D[i]) {
+			max = D[i];
+		}
+	}
+
+	cout << max;
+}
